7f0d2b7c8b7c80a80f7666f791f04de2c09b89e5
brutussalesllc <brutussalesllc@gmail.com>
2025-06-27 11:01:08 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🚀 Card AI is working!</h1>
+      <p>This is your functional test build. Time to scan some cards!</p>
+    </div>
+  )
+}
+
+export default App
